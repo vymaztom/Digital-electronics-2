1bb7625ca577afd27cad95d9a211cc8e54a8fc84
vymaztom <vyma.96@gmail.com>
2020-09-29 09:09:48 +0200

    removed coments

--- Labs/01-tools/main.c
+++ Labs/01-tools/main.c
@@ -9,34 +9,22 @@
  *
  **********************************************************************/
 
-#define LED_GREEN   PB5     // AVR pin where green LED is connected
+#define LED_GREEN   PB5
 #define SHORT_DELAY 100
 #define DELAY 500
 #define LONG_DELAY 1000
 
 #ifndef F_CPU
-#define F_CPU 16000000      // CPU frequency in Hz required for delay func
+#define F_CPU 16000000
 #endif
-
 
 #include <util/delay.h>
 #include <avr/io.h>
 
-
 int main(void){
-    // Set pin as output in Data Direction Register
-    // DDRB = DDRB or 0010 0000
     DDRB = DDRB | (1<<LED_GREEN);
-
-    // Set pin LOW in Data Register (LED off)
-    // PORTB = PORTB and 1101 1111
     PORTB = PORTB & ~(1<<LED_GREEN);
-
-    // Infinite loop
     while (1){
-
-		// CALL SOS IN MORSE  ///.../---/....///
-
 		for(int i = 0 ; i < 3 ; i++){
 			for(int j = 0 ; j < 3 ; j++){
 				PORTB = PORTB ^ (1<<LED_GREEN);
@@ -51,6 +39,5 @@ int main(void){
 		}
 		_delay_ms(LONG_DELAY);
     }
-
     return 0;
 }
---
+++ Labs/simpleProject/simpleProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * simpleProject.c
+ *
+ * Created: 29.09.2020 8:34:32
+ * Author : vyma9
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
